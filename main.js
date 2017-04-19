4b5d51f8f5e83164650d7c7ca0c3e4e607610304
joehathaway <josephhathaway@amdg.rockhursths.edu>
2017-04-19 11:24:30 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var decc = true;
+var dec = true;
 var WIDTH = 400;
 var HEIGHT = 400;
 setSize(WIDTH, HEIGHT);
@@ -6,45 +6,100 @@ setSize(WIDTH, HEIGHT);
 var WINNING_LINE_WIDTH = 10;
 var WINNING_LINE_COLOR = Color.red;
 
-function start(){
-    
-    
-    
+var board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-    
-     mouseClickMethod(add);
+
+
+
+
+function start(){
+    init();
+    mouseClickMethod(check);
 }
 
-function grid(){
-    
-    var a = new Rectangle(getWidth(),2);
-    a.setColor(Color.black);
-    a.setPosition(0, 2 * getWidth() / 3);
+function init(){
+    var a = new Rectangle(2, getWidth());
+    var b = new Rectangle(2, getWidth());
+    var c = new Rectangle(getHeight(), 2);
+    var d = new Rectangle(getHeight(), 2);
+    a.setPosition(getWidth() / 3, 0);
+    b.setPosition(2 * getWidth() / 3, 0);
+    c.setPosition(0, getHeight() / 3);
+    d.setPosition(0, 2 * getHeight() / 3);
     add(a);
-    
-    var b = new Rectangle(getWidth(),2);
-    b.setColor(Color.black);
-    b.setPosition(0, getWidth() / 3);
     add(b);
-    
-    var c = new Rectangle(2, getHeight());
-    c.setColor(Color.black);
-    c.setPosition(getHeight() / 3, 0);
     add(c);
-    
-    var d = new Rectangle(2, getHeight());
-    d.setColor(Color.black);
-    d.setPosition(2 * getHeight() / 3, 0);
     add(d);
 }
 
-function add(e) {
-    if(decc == true){
-        var x = new Text("X", "Arial 70pt");
-        decc = false;
-    }else{
-        var x = new Text("O", "Arial 70pt");
-        decc = true;
+function check(e){
+    if(dec == true){
+        var x = new Text("X", "80pt Arial");
+        dec = false;
+        var y = 1;
+    }else if(dec == false){
+        var x = new Text("O", "80pt Arial");
+        dec = true;
+        var y = 2;
     }
     
+    if(e.getX() > 0 && e.getX() < getWidth() / 3){
+        if(e.getY() > 0 && e.getY() < getHeight() / 3 && board[0] == 0){
+            x.setPosition(30, 100);
+            add(x);
+            board[0] = y;
+        } 
+        if(e.getY() > getHeight() /3 && e.getY() < 2 * getHeight() / 3 && board[3] == 0){
+            x.setPosition(30, 225);
+            add(x);
+            board[3] = y;
+        } 
+        if(e.getY() > 2 * getHeight() / 3 && e.getY() < 3 * getHeight() / 3 && board[6] == 0){
+            x.setPosition(30, 375);
+            add(x);
+            board[6] = y;
+        } 
+    } 
+    if(e.getX() > getWidth() / 3 && e.getX() < 2 * getWidth() / 3){
+        if(e.getY() > 0 && e.getY() < getHeight() / 3 && board [1] == 0){
+            x.setPosition(170, 100);
+            add(x);
+            board[1] = y;
+        } 
+        if(e.getY() > getHeight() /3 && e.getY() < 2 * getHeight() / 3 && board[4] == 0){
+            x.setPosition(165, 225);
+            add(x);
+            board[4] = y;
+        } 
+        if(e.getY() > 2 * getHeight() / 3 && e.getY() < 3 * getHeight() / 3 && board[7] == 0){
+            x.setPosition(170, 375);
+            add(x);
+            board[7] = y;
+        } 
+        
+        
+    } 
+    if(e.getX() > 2 * getWidth() / 3 && e.getX() < 3 * getWidth() / 3){
+        if(e.getY() > 0 && e.gestY() < getHeight() / 3 && board [2] == 0){
+            x.setPosition(290, 100);
+            add(x);
+            board[2] = y;
+        } 
+        if(e.getY() > getHeight() /3 && e.getY() < 2 * getHeight() / 3 && board[5] == 0){
+            x.setPosition(290, 225);
+            add(x);
+            board[5] = y;
+        } 
+        if(e.getY() > getHeight() / 3 * 2 && e.getY() < getHeight() && board [8] == 0){
+            x.setPosition(290, 375);
+            add(x);
+            board[8] = y;
+        } 
+        
+        
+    } 
+    
+   
+    
 }
+s
